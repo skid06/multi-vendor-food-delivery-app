b548520ec19a6e153a6867caa068f89a2fb23631
Melchor Valencia <valencia.mel.06@gmail.com>
2025-01-30 13:19:47 +0800

    Setup the relationship for OrderItem model.

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'order_id',
+        'food_item_id',
+        'price',
+        'quantity',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function foodItem()
+    {
+        return $this->belongsTo(FoodItem::class);
+    }
+}

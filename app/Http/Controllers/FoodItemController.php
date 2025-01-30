1c221e9755fcbbc41ea3b9dffa42c75f07ff6156
Melchor Valencia <valencia.mel.06@gmail.com>
2025-01-30 18:23:29 +0800

    Create FoodItemController and add basic functionality related to FoodItem.

---
+++ app/Http/Controllers/FoodItemController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FoodItem;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class FoodItemController extends Controller
+{
+    public function index(Restaurant $restaurant)
+    {
+        return $restaurant->foodItems;
+    }
+
+    public function store(Request $request, Restaurant $restaurant)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'image' => 'nullable|string',
+        ]);
+
+        $foodItem = $restaurant->foodItems()->create($request->all());
+        return response()->json($foodItem, 201);
+    }
+
+    public function show(FoodItem $foodItem)
+    {
+        return $foodItem;
+    }
+
+    public function update(Request $request, FoodItem $foodItem)
+    {
+        $foodItem->update($request->all());
+        return response()->json($foodItem, 200);
+    }
+
+    public function destroy(FoodItem $foodItem)
+    {
+        $foodItem->delete();
+        return response()->json(null, 204);
+    }
+}

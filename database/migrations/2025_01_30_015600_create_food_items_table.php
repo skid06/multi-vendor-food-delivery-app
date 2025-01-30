fbab633ac72ba13f35e2d950acd640b659fa879f
Melchor Valencia <valencia.mel.06@gmail.com>
2025-01-30 10:40:15 +0800

    Setup the schema for users, food items, orders and restaurants.

--- database/migrations/2025_01_30_015600_create_food_items_table.php
+++ database/migrations/2025_01_30_015600_create_food_items_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('food_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('restaurant_id')->constrained()->onDelete('cascade'); // Restaurant associated with the order
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2); // Total amount of the order
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2025_01_30_015600_create_restaurants_table.php
+++ database/migrations/2025_01_30_015600_create_restaurants_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('address');
+            $table->string('phone');
+            $table->string('logo')->nullable();
             $table->timestamps();
         });
     }

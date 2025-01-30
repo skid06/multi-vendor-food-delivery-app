43344ac441a9e2dcb01913ab63191ba049256775
Melchor Valencia <valencia.mel.06@gmail.com>
2025-01-30 13:18:20 +0800

    Setup the relationship for Order model.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'restaurant_id',
+        'status',
+        'total_amount',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }

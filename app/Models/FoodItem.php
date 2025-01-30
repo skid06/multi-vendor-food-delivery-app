92eff2630f95795897e6461be9ea35af0df21b03
Melchor Valencia <valencia.mel.06@gmail.com>
2025-01-30 13:10:12 +0800

    Setup the relationship for FoodItem.

--- app/Models/FoodItem.php
+++ app/Models/FoodItem.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class FoodItem extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'restaurant_id',
+        'price',
+        'image'
+    ];
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }

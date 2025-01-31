6f6b8646a4139946d66ab1ed3006d03a00015a2f
Melchor Valencia <valencia.mel.06@gmail.com>
2025-01-31 20:54:45 +0800

    Enables the factory to the Restaurant model.

---
+++ database/factories/RestaurantFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Restaurant;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Restaurant>
+ */
+class RestaurantFactory extends Factory
+{
+    protected $model = Restaurant::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'description' => fake()->text(),
+            'address' => fake()->address(),
+            'phone' => fake()->phoneNumber(),
+            'user_id' => User::factory(),
+        ];
+    }
+}

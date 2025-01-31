7fd2455911bf3958c1fee5e90d068dd83cbe4167
Melchor Valencia <valencia.mel.06@gmail.com>
2025-01-31 09:20:47 +0800

    Registers the binding of PaymentGateway in the AppServiceProvider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Contracts\PaymentGateway;
+use App\Services\StripePaymentGatewayService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(PaymentGateway::class, StripePaymentGateway::class);
     }
 
     /**

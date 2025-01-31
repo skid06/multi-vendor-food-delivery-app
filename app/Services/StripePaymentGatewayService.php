2dbc316dde332bf8b94f2b5559e4ac8920934597
Melchor Valencia <valencia.mel.06@gmail.com>
2025-01-31 09:11:17 +0800

    Create a StripePaymentService that will implement PaymentGateway for payment integration.

---
+++ app/Services/StripePaymentGatewayService.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Services;
+
+use Stripe\Stripe;
+use App\Contracts\PaymentGateway;
+
+class StripePaymentGatewayService implements PaymentGateway
+{
+    public function __construct()
+    {
+        Stripe::setApiKey(config('services.stripe.secret'));
+    }
+
+    public function charge($amount, $token)
+    {
+        try {
+            $charge = \Stripe\Charge::create([
+                'amount' => $amount * 100, // Convert to cents
+                'currency' => 'usd',
+                'source' => $token,
+            ]);
+
+            return $charge->id;
+        } catch (\Exception $e) {
+            throw new \Exception("Payment failed: " . $e->getMessage());
+        }
+    }
+}

12b150df0b496fd364727ed0a02abc9eebc01716
Melchor Valencia <valencia.mel.06@gmail.com>
2025-02-01 11:49:32 +0800

    Creates and applies factory for FoodItem.

--- app/Models/FoodItem.php
+++ app/Models/FoodItem.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FoodItem extends Model
 {
+    use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
---
+++ database/factories/FoodItemFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Restaurant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\FoodItem>
+ */
+class FoodItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'description' => fake()->text(),
+            'price' => fake()->numberBetween(100, 1000),
+            'restaurant_id' => Restaurant::factory(),
+        ];
+    }
+}

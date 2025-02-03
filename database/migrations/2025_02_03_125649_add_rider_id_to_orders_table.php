62478ceb058c66e9b665ac6c836165e97aab09e8
Melchor Valencia <valencia.mel.06@gmail.com>
2025-02-03 20:57:39 +0800

    Add rider_id to orders table.

---
+++ database/migrations/2025_02_03_125649_add_rider_id_to_orders_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRiderIdToOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('rider_id')->nullable()->constrained('users')->onDelete('set null');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['rider_id']);
+            $table->dropColumn('rider_id');
+        });
+    }
+}

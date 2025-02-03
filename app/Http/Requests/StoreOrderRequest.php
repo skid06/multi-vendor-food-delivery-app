a7ab53baac78720f84d9accc0ddeb73e255bceaf
Melchor Valencia <valencia.mel.06@gmail.com>
2025-02-03 10:58:16 +0800

    Creates a form request for Order.

---
+++ app/Http/Requests/StoreOrderRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // Return true if the user is authorized to make this request
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'restaurant_id' => 'required|exists:restaurants,id',
+            'items' => 'required|array',
+            'items.*.food_item_id' => 'required|exists:food_items,id',
+            'items.*.quantity' => 'required|integer|min:1',
+            'payment_token' => 'required|string',
+        ];
+    }
+}

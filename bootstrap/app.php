0543b29b07d478d426d94539e05b71ac8621e0e4
Melchor Valencia <valencia.mel.06@gmail.com>
2025-02-03 20:29:27 +0800

    Unregisters the RoleMiddleware as it only needs to use directly on route.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(\App\Http\Middleware\RoleMiddleware::class);
+//        $middleware->append(\App\Http\Middleware\RoleMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
